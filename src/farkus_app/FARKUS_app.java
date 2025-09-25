5dcac28a540d3c2f62a18245a4d9ef54d411524b
Vicente Miqueles LOCAl <vicente.miqueles.palma@alumnos.uta.cl>
2025-09-25 02:04:21 -0300

    Primeer commit

---
+++ src/farkus_app/FARKUS_app.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package farkus_app;
+
+/**
+ *
+ * @author vicente
+ */
+public class FARKUS_app {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
